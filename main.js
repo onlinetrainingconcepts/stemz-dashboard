4f68d66a2207f7f7ca0112458a38420c5e32e6ec
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2023-10-06 16:14:58 -0400

    feat: refactor javascript

--- main.js
+++ main.js
@@ -1,34 +1,46 @@
-// variables
-const tabButtons = document.querySelectorAll('.dashboard__nav--button');
-const headerText = document.querySelector('.dashboard__header--text');
-const tabContent = document.querySelectorAll('.tabcontent');
+function initializeTabs() {
+	document.querySelectorAll('.dashboard__nav--button').forEach((button) => {
+		button.addEventListener('click', () => {
+			// variables
+			const nav = button.parentElement;
+			const contentContainer = nav.parentElement.parentElement.childNodes[11];
+			const tabNumber = button.dataset.forTab;
+			const tabToActivate = contentContainer.querySelector(
+				`.tabcontent[data-tab="${tabNumber}"]`
+			);
+			const header = contentContainer.parentElement.querySelector(
+				'.dashboard__header--text'
+			);
+			const headerText = button.querySelector(
+				'.dashboard__nav--button-text'
+			).innerText;
 
-function initializeDefaultView() {
-	// ensures the first tab is actie when page loads
-	tabButtons[0].classList.add('active');
-	// hides all the tab content sections except the first one
-	tabContent.forEach((tab, index) => {
-		if (index > 0) {
-			tab.style.display = 'none';
-		}
+			console.log(header);
+
+			// removes active status on all buttons
+			nav.querySelectorAll('.dashboard__nav--button').forEach((button) => {
+				button.classList.remove('active');
+			});
+
+			// removes active status on all tabs
+			contentContainer.querySelectorAll('.tabcontent').forEach((tab) => {
+				tab.classList.remove('tabcontent--active');
+			});
+
+			// adds active class to selected button
+			button.classList.add('active');
+
+			// adds active class to associated tab
+			tabToActivate.classList.add('tabcontent--active');
+
+			// adds text to header
+			header.innerText = headerText;
+		});
 	});
 }
 
-initializeDefaultView();
-
-function openTab(e, tab) {
-	// upon click, hides all content
-	tabContent.forEach((div) => {
-		div.style.display = 'none';
-	});
-	// upon click, removes active classes of tab buttons
-	tabButtons.forEach((nav) => {
-		nav.classList.remove('active');
-	});
-	// shows current content div
-	document.getElementById(tab).style.display = 'block';
-	// adds active class to current tab button
-	e.currentTarget.classList.add('active');
-	// changes header text
-	headerText.innerText = e.currentTarget.innerText;
-}
+document.addEventListener('DOMContentLoaded', () => {
+	initializeTabs();
+	// clicks the first button so inital tab will show on default
+	document.querySelector('.dashboard__nav--button').click();
+});
