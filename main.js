e87de51d5b96d5d728986b4c1eef4d02e7b1fcf8
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2023-10-09 10:52:02 -0400

    feat: convert js events to class based solution

--- main.js
+++ main.js
@@ -1,46 +1,57 @@
-function initializeTabs() {
-	document.querySelectorAll('.dashboard__nav--button').forEach((button) => {
-		button.addEventListener('click', () => {
-			// variables
-			const nav = button.parentElement;
-			const contentContainer = nav.parentElement.parentElement.childNodes[11];
-			const tabNumber = button.dataset.forTab;
-			const tabToActivate = contentContainer.querySelector(
-				`.tabcontent[data-tab="${tabNumber}"]`
-			);
-			const header = contentContainer.parentElement.querySelector(
-				'.dashboard__header--text'
-			);
-			const headerText = button.querySelector(
-				'.dashboard__nav--button-text'
-			).innerText;
+class Dashboard {
+	constructor(dashboard, dashboardName) {
+		this.dashboard = dashboard;
+		this.dashboardName = dashboardName;
+		const domDashboard = document.getElementById(dashboard.id);
+		const nav = domDashboard.querySelector('.dashboard__nav');
+		const contentContainer = domDashboard.querySelector('.dashboard__content');
 
-			console.log(header);
+		domDashboard
+			.querySelectorAll(`.dashboard__nav--button`)
+			.forEach((button) => {
+				button.addEventListener('click', () => {
+					// variables
+					const tabNumber = button.dataset.forTab;
+					const tabToActivate = contentContainer.querySelector(
+						`.tabcontent[data-tab="${tabNumber}"]`
+					);
+					const header = contentContainer.parentElement.querySelector(
+						'.dashboard__header--text'
+					);
+					const headerText = button.querySelector(
+						'.dashboard__nav--button-text'
+					).innerText;
 
-			// removes active status on all buttons
-			nav.querySelectorAll('.dashboard__nav--button').forEach((button) => {
-				button.classList.remove('active');
+					// removes active status on all buttons
+					nav.querySelectorAll('.dashboard__nav--button').forEach((button) => {
+						button.classList.remove('active');
+					});
+
+					// removes active status on all tabs
+					contentContainer.querySelectorAll('.tabcontent').forEach((tab) => {
+						tab.classList.remove('tabcontent--active');
+					});
+
+					// adds active class to selected button
+					button.classList.add('active');
+
+					// adds active class to associated tab
+					tabToActivate.classList.add('tabcontent--active');
+
+					// adds text to header
+					header.innerText = headerText;
+				});
 			});
 
-			// removes active status on all tabs
-			contentContainer.querySelectorAll('.tabcontent').forEach((tab) => {
-				tab.classList.remove('tabcontent--active');
-			});
-
-			// adds active class to selected button
-			button.classList.add('active');
-
-			// adds active class to associated tab
-			tabToActivate.classList.add('tabcontent--active');
-
-			// adds text to header
-			header.innerText = headerText;
+		// clicks the first button in the nav in order to have the first tab show by default
+		document.addEventListener('DOMContentLoaded', () => {
+			domDashboard.querySelector('.dashboard__nav--button').click();
 		});
-	});
+	}
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-	initializeTabs();
-	// clicks the first button so inital tab will show on default
-	document.querySelector('.dashboard__nav--button').click();
+const dashboards = [];
+
+document.querySelectorAll('.dashboard').forEach((dashboard, num) => {
+	dashboards[num] = new Dashboard(dashboard, dashboard.dataset.dashboard);
 });
